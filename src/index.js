00a5a3490990ade78abefca1cf253564d9d608ff
malashonock <malashonock@gmail.com>
2021-08-21 16:22:44 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let digit = 0;
+    let reversedNumber = 0;
+
+    n = Math.abs(n);
+
+    while (n) {
+        digit = n % 10;
+        reversedNumber = reversedNumber * 10 + digit;
+        n = Math.floor(n / 10);
+    }
+    
+    return reversedNumber;
 }
